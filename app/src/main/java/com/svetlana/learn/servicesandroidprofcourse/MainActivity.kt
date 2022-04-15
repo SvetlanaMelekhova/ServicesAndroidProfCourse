cdc46acc4a046a0e0204ce2183e193c9eeb922c5
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-04-15 13:18:25 +0300

    job service with enqueue

--- app/src/main/java/com/svetlana/learn/servicesandroidprofcourse/MainActivity.kt
+++ app/src/main/java/com/svetlana/learn/servicesandroidprofcourse/MainActivity.kt
@@ -2,7 +2,9 @@ package com.svetlana.learn.servicesandroidprofcourse
 
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
 import android.content.ComponentName
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -35,17 +37,19 @@ class MainActivity : AppCompatActivity() {
 
         //since api 29 doesn't work with setRequiresCharging and setRequiredNetworkType
         binding.jobDispatcher.setOnClickListener {
-            val componentName = ComponentName(this, MyJobService::class.java)
+            val componentName = ComponentName(this, MyJobServiceEnqueue::class.java)
 
-            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
-                .setExtras(MyJobService.newBundle(page++))
-                .setRequiresCharging(true)
-                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+            val jobInfo = JobInfo.Builder(MyJobServiceEnqueue.JOB_ID, componentName)
+                //.setExtras(MyJobService.newBundle(page++))
+                //.setRequiresCharging(true)
+                //.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
                 .setPersisted(true)
                 .build()
 
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(jobInfo)
+            //jobScheduler.schedule(jobInfo)
+            val intent = MyJobServiceEnqueue.newIntent(page++)
+            jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
         }
     }
 }
