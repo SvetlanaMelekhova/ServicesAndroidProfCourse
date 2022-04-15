dc729d834420c85e8d11a4f5df91a8eb1f8daf23
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-04-15 12:46:19 +0300

    job service

--- app/src/main/java/com/svetlana/learn/servicesandroidprofcourse/MainActivity.kt
+++ app/src/main/java/com/svetlana/learn/servicesandroidprofcourse/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.svetlana.learn.servicesandroidprofcourse
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -10,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var page = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -25,6 +31,21 @@ class MainActivity : AppCompatActivity() {
 
         binding.intentService.setOnClickListener {
             ContextCompat.startForegroundService(this, MyIntentService.newIntent(this))
+        }
+
+        //since api 29 doesn't work with setRequiresCharging and setRequiredNetworkType
+        binding.jobDispatcher.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setExtras(MyJobService.newBundle(page++))
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(jobInfo)
         }
     }
 }
---
+++ app/src/main/java/com/svetlana/learn/servicesandroidprofcourse/MyJobService.kt
@@ -0,0 +1,57 @@
+package com.svetlana.learn.servicesandroidprofcourse
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.os.PersistableBundle
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService : JobService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartCommand")
+        val page = params?.extras?.getInt(PAGE) ?: 0
+        coroutineScope.launch {
+            for (i in 0 until 5) {
+                delay(1000)
+                log("Timer $i $page")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyJobService: $message")
+    }
+
+    companion object {
+
+        const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newBundle(page: Int): PersistableBundle {
+            return PersistableBundle().apply {
+                putInt(PAGE, page)
+            }
+        }
+    }
+}
